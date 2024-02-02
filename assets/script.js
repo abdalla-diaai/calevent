b8e2df99d71b927e4f32402ac383e83309b0e4a7
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-02-02 13:56:28 +0000

    search results table

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,59 @@ adeleUrl = `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K
 
 function ticketFetch() {
     fetch(adeleUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // for (var i = 0; i < 10; i++){
-        //     console.log(data)
-        // }
-        console.log(data)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            
+            var eventName = data._embedded.events[0].name;
+            console.log(eventName)
 
-})
+            var eventVenue = data._embedded.events[0]._embedded.venues[0].name;
+            console.log(eventVenue);
+
+            var eventDate = data._embedded.events[0].dates.start.localDate;
+            console.log(eventDate);
+
+            var eventPrice = data._embedded.events[0].priceRanges[0].min;
+            console.log(eventPrice)
+
+
+            for (var i = 0; i < 10; i++) {
+                console.log(data)
+            }
+        
+            console.log(data)
+
+            // create table element to display search results
+            var resultsTable = $("<table/>");
+            var headerRow = $("<tr/>");
+            headerRow.append($("<th>").text("Event"));
+            headerRow.append($("<th>").text("Venue"));
+            headerRow.append($("<th>").text("Date"));
+            headerRow.append($("<th>").text("Price"));
+            headerRow.append($("<th>").text("Save"));
+
+            // create table rows, text value will be changed later to show results data
+            for (var i = 0; i < 10; i++) {
+                var tableRow = $("<tr/>");
+                tableRow.append($("<td>").text(eventName));
+                tableRow.append($("<td>").text(eventVenue));
+                tableRow.append($("<td>").text(eventDate));
+                // format price to US dollars
+                var formattedPrice = "$" + eventPrice.toFixed(2);
+                tableRow.append($("<td>").text(formattedPrice));
+                resultsTable.append(tableRow);
+            }
+
+            resultsTable.prepend(headerRow);
+            // append table to main page body
+            $("#results-table").append(resultsTable);
+        })
 }
 
+// call ticketFetch function
 ticketFetch()
+
+
+
