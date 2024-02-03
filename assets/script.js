4469221e8c890925ffb2c2e27689c983dbd9cbaf
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-02-03 19:40:34 +0000

    organised html, added CORS link to js, added random jokes api.

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,17 @@
 apiKey = "hA8Tg18Yh6X3uiQW5tD5GjoRkXjrJlsl";
 
-queryUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}`;
+queryUrl = `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${apiKey}`;
 
-adeleUrl = `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&countryCode=US&apikey=${apiKey}`;
+// to solve CORS issues
+const url = "https://cors-anywhere-jung-48d4feb9d097.herokuapp.com/" + queryUrl
 
 function ticketFetch() {
-  fetch(adeleUrl)
+  fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      var eventName = data._embedded.events[0].name;
-      console.log(eventName);
-
-      var eventVenue = data._embedded.events[0]._embedded.venues[0].name;
-      console.log(eventVenue);
-
-      var eventDate = data._embedded.events[0].dates.start.localDate;
-      console.log(eventDate);
-
-      var eventPrice = data._embedded.events[0].priceRanges[0].min;
-      console.log(eventPrice);
-
-      for (var i = 0; i < 10; i++) {
-        console.log(data);
-      }
-
-      console.log(data);
-
-      // create table element to display search results
+    //   create table element to display search results
       var resultsTable = $("<table/>");
       var headerRow = $("<tr/>");
       headerRow.append($("<th>").text("Event"));
@@ -40,12 +23,15 @@ function ticketFetch() {
       // create table rows, text value will be changed later to show results data
       for (var i = 0; i < 10; i++) {
         var tableRow = $("<tr/>");
-        tableRow.append($("<td>").text(eventName));
-        tableRow.append($("<td>").text(eventVenue));
-        tableRow.append($("<td>").text(eventDate));
-        // format price to US dollars
-        var formattedPrice = "$" + eventPrice.toFixed(2);
-        tableRow.append($("<td>").text(formattedPrice));
+        tableRow.append($("<td>").text(data._embedded.events[i].name));
+        tableRow.append($("<td>").text(data._embedded.events[i]._embedded.venues[0].name));
+        tableRow.append($("<td>").text(data._embedded.events[i].dates.start.localDate));
+        // check if price is available or not available if not
+        try {
+            tableRow.append($("<td>").text(`$${data._embedded.events[i].priceRanges[0].min}`));
+        } catch(err) {
+            tableRow.append($("<td>").text('Not Available'))
+        }
         resultsTable.append(tableRow);
       }
 
@@ -58,6 +44,24 @@ function ticketFetch() {
 // call ticketFetch function
 
 ticketFetch();
+
+// jokes fetch
+
+var jokesUrl = 'https://official-joke-api.appspot.com/random_joke'
+
+function jokeFetch() {
+    fetch(jokesUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        document.querySelector('.accordion-button').innerHTML = data.setup;
+        document.querySelector('.accordion-body').innerHTML = data.punchline;
+      });
+  }
+
+jokeFetch()
+
 
 const burgerMenu = document.querySelector(".burger-icon svg");
 
@@ -72,3 +76,4 @@ crossIcon.addEventListener("click", function () {
   const sideBar = document.querySelector(".sidebar");
   sideBar.style.display = "none";
 });
+
